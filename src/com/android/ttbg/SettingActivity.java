a6ce074288e4ca6e61c847c707e7739ffc2c069b
jockey <wyxmail@163.com>
2017-03-22 18:44:01 +0800

    设置亮度界面,动态显示开关

--- src/com/android/ttbg/SettingActivity.java
+++ src/com/android/ttbg/SettingActivity.java
@@ -1,15 +1,26 @@
 package com.android.ttbg;
 
+
+
+import com.android.ttbg.util.OperatingSharedPreferences;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.ToggleButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class SettingActivity extends Activity {
 	
     private ImageView  title_back;
     private View item_setting_edit;
+    
+    private ToggleButton toggle_switch;
+    boolean toggle_switchEnable = true;
+    private View rly_setting_light;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -31,7 +42,37 @@ public class SettingActivity extends Activity {
 					startActivity(intent);
 		        }  
 		  }); 
+		 
+		 rly_setting_light = (View)findViewById(R.id.rly_setting_light);
+		 boolean bOn = OperatingSharedPreferences.getLightSetting(SettingActivity.this);
+		 
+		 if(bOn)
+		 {
+			 rly_setting_light.setVisibility(View.VISIBLE);
+		 }
+		 else
+		 {
+			 rly_setting_light.setVisibility(View.GONE);
+		 }
+		 toggle_switch  = (ToggleButton)findViewById(R.id.toggle_switch);
+		 toggle_switch.setChecked(bOn);
+		 toggle_switch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
+				@Override
+				public void onCheckedChanged(CompoundButton buttonView,
+						boolean isChecked) {
+					// TODO Auto-generated method stub
+					OperatingSharedPreferences.setLightSetting(SettingActivity.this,isChecked);
+					 if(isChecked)
+					 {
+						 rly_setting_light.setVisibility(View.VISIBLE);
+					 }
+					 else
+					 {
+						 rly_setting_light.setVisibility(View.GONE);
+					 }
+				}
+		 });
 	}
 	
 	
--- src/com/android/ttbg/util/OperatingSharedPreferences.java
+++ src/com/android/ttbg/util/OperatingSharedPreferences.java
@@ -13,8 +13,11 @@ public class OperatingSharedPreferences {
 	public static final String PREFERENCE_FIRSTOPEN="FIRSTOPEN";
 	public static final String PREFERENCE_USER_IMAGE="AVATAR";
 	
+	//个人资料里用户名称
 	public static final String PREFERENCE_SETTING_USER_NAME="USER_NAME";
-	
+	//设置里亮度调节
+	public static final String PREFERENCE_SETTING_LIGHT_SETTING="LIGHT_SETTING";
+	public static final boolean PREFERENCE_SETTING_LIGHT_SETTING_DEFAULT=false;
     /**  
      * <pre>  
      * 基本功能：保存启动标识到SharedPreferences  
@@ -25,9 +28,9 @@ public class OperatingSharedPreferences {
      */  
     public static void setBooleanFirstBoot(Context context) {  
         SharedPreferences sharedPreferences = context.getSharedPreferences(  
-        		OperatingSharedPreferences.PREFERENCE_NAME, Context.MODE_PRIVATE);  
+        		PREFERENCE_NAME, Context.MODE_PRIVATE);  
         Editor editor = sharedPreferences.edit();// 获取编辑器  
-        editor.putBoolean(OperatingSharedPreferences.PREFERENCE_FIRSTOPEN, false);  
+        editor.putBoolean(PREFERENCE_FIRSTOPEN, false);  
         editor.commit();// 提交修改  
     }  
   
@@ -41,33 +44,46 @@ public class OperatingSharedPreferences {
      */  
     public static boolean getBooleanFirstBoot(Context context) {  
         SharedPreferences sharedPreferences = context.getSharedPreferences(  
-        		OperatingSharedPreferences.PREFERENCE_NAME, Context.MODE_PRIVATE);  
+        		PREFERENCE_NAME, Context.MODE_PRIVATE);  
         // getString()第二个参数为缺省值，如果preference中不存在该key，将返回缺省值true  
-        boolean firstopen = sharedPreferences.getBoolean(OperatingSharedPreferences.PREFERENCE_FIRSTOPEN, true);  
+        boolean firstopen = sharedPreferences.getBoolean(PREFERENCE_FIRSTOPEN, true);  
         return firstopen;  
     }  
     
     public static void saveUserImage(Context context,String urlpath) {  
     SharedPreferences sharedPreferences = context.getSharedPreferences(  
-        		OperatingSharedPreferences.PREFERENCE_NAME, Context.MODE_PRIVATE);  
+        		PREFERENCE_NAME, Context.MODE_PRIVATE);  
     Editor editor = sharedPreferences.edit();// 获取编辑器  
-    editor.putString(OperatingSharedPreferences.PREFERENCE_USER_IMAGE, urlpath);
+    editor.putString(PREFERENCE_USER_IMAGE, urlpath);
     editor.commit();
     }
     
     public static void saveUserName(Context context,String name) {  
     SharedPreferences sharedPreferences = context.getSharedPreferences(  
-        		OperatingSharedPreferences.PREFERENCE_NAME, Context.MODE_PRIVATE);  
+        		PREFERENCE_NAME, Context.MODE_PRIVATE);  
     Editor editor = sharedPreferences.edit();
-    editor.putString(OperatingSharedPreferences.PREFERENCE_SETTING_USER_NAME, name);
+    editor.putString(PREFERENCE_SETTING_USER_NAME, name);
     editor.commit();
     }
     public static String getUserName(Context context) {  
         SharedPreferences sharedPreferences = context.getSharedPreferences(  
-        		OperatingSharedPreferences.PREFERENCE_NAME, Context.MODE_PRIVATE);  
-        String username = sharedPreferences.getString(OperatingSharedPreferences.PREFERENCE_SETTING_USER_NAME, "");  
+        		PREFERENCE_NAME, Context.MODE_PRIVATE);  
+        String username = sharedPreferences.getString(PREFERENCE_SETTING_USER_NAME, "");  
         return username;  
     }  
     
+    public static void setLightSetting(Context context,Boolean bOn) {  
+    SharedPreferences sharedPreferences = context.getSharedPreferences(  
+        		PREFERENCE_NAME, Context.MODE_PRIVATE);  
+    Editor editor = sharedPreferences.edit();
+    editor.putBoolean(PREFERENCE_SETTING_LIGHT_SETTING, bOn);
+    editor.commit();
+    }
+    public static boolean getLightSetting(Context context) {  
+        SharedPreferences sharedPreferences = context.getSharedPreferences(  
+        		OperatingSharedPreferences.PREFERENCE_NAME, Context.MODE_PRIVATE);  
+        Boolean bOn = sharedPreferences.getBoolean(PREFERENCE_SETTING_LIGHT_SETTING, PREFERENCE_SETTING_LIGHT_SETTING_DEFAULT);  
+        return bOn;  
+    }  
     
 }  
