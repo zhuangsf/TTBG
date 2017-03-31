497b453434be7da59929b3f7f3618906b0b2815f
jockey <jockey@DESKTOP-I8D9NV0>
2017-03-31 23:42:50 +0800

    先提交,用于显示联系人信息

---
+++ src/com/android/ttbg/view/AddressItem.java
@@ -0,0 +1,11 @@
+package com.android.ttbg.view;
+
+public class AddressItem {
+	private int id;
+	private String name;
+	private String phone;
+	private String aera;
+	private String address;
+	private String code;
+	private Boolean bDefault;
+}
