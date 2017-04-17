ddab044af9939f4fe84678411f37f73ee961d968
jockey <jockey@DESKTOP-I8D9NV0>
2017-04-17 23:17:37 +0800

    搜索记录显示

--- src/com/android/ttbg/SearchActivity.java
+++ src/com/android/ttbg/SearchActivity.java
@@ -44,17 +44,29 @@ public class SearchActivity extends ActivityPack {
 	private static final String TAG = "SearchActivity";
     private ImageView  title_back;
     private TextView title_tv_search;
+    private TextView bt_search_clear_his;
     private ImageView add_menus;
     private Context mContext;
     private EditTextWithDel title_et_search;
     private SimpleAdapter simpleAdapter;
+    private ListView lv;
+    private List<Map<String,Object>> history_data=new ArrayList<Map<String,Object>>();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
 
 		mContext = SearchActivity.this;
-		
+		bt_search_clear_his  = (TextView)findViewById(R.id.bt_search_clear_his);
+		bt_search_clear_his.setOnClickListener(new View.OnClickListener() {  
+	        public void onClick(View v) {  
+	        	OperatingSP.setString(mContext, OperatingSP.PREFERENCE_SEARCH_RECORD,  OperatingSP.PREFERENCE_SEARCH_RECORD_DEFAULT);
+	        	history_data.clear();
+	        	bt_search_clear_his.setVisibility(View.GONE);
+        		simpleAdapter.notifyDataSetChanged();
+        		setListViewHeightBasedOnChildren(lv);  
+	        }  
+	  }); 
 		title_et_search = (EditTextWithDel)findViewById(R.id.title_et_search);
 		
 		title_tv_search = (TextView)findViewById(R.id.title_tv_search);
@@ -67,12 +79,37 @@ public class SearchActivity extends ActivityPack {
 		        		//保存
 		        		StringBuilder stringBuilder = new StringBuilder();  
 		        		String historyRecords = OperatingSP.getString(mContext, OperatingSP.PREFERENCE_SEARCH_RECORD, OperatingSP.PREFERENCE_SEARCH_RECORD_DEFAULT);
-		        		stringBuilder.append(historyRecords);
-		        		stringBuilder.append("_");
+		        		if(!historyRecords.equals(""))
+		        		{
+		        			stringBuilder.append(historyRecords);
+		        		}
 		        		stringBuilder.append(title_et_search.getText().toString());
+		        		stringBuilder.append("#");
 		        		OperatingSP.setString(mContext, OperatingSP.PREFERENCE_SEARCH_RECORD, stringBuilder.toString());
 		        		
+		        		Utils.Log("stringBuilder = :"+stringBuilder.toString());
+
+	        			String[] arrayRecords = stringBuilder.toString().split("#");  //用#当分割符
+	        		
+	        			history_data.clear();
+	        			for(int i = 0;i < arrayRecords.length;i++)
+	        			{
+	        				Utils.Log("arrayRecords["+i+"]  = :"+arrayRecords[i]);
+	        				Map<String,Object> map1=new HashMap<String, Object>();
+	        				map1.put("item_history",arrayRecords[i]);
+	        				history_data.add(map1);
+	        			}
+	        			if(history_data.size() == 0)
+	        			{
+	        				bt_search_clear_his.setVisibility(View.GONE);
+	        			}
+	        			else
+	        			{
+	        				bt_search_clear_his.setVisibility(View.VISIBLE);
+	        			}
+		        		
 		        		simpleAdapter.notifyDataSetChanged();
+		        		setListViewHeightBasedOnChildren(lv);   
 		        	}
 		        }  
 		  }); 
@@ -80,8 +117,8 @@ public class SearchActivity extends ActivityPack {
 		initListView();
 		
 
-		 
-		 
+
+
 		 title_back = (ImageView)findViewById(R.id.title_back);
 		 title_back.setOnClickListener(new View.OnClickListener() {  
 		        public void onClick(View v) {  
@@ -97,21 +134,31 @@ public class SearchActivity extends ActivityPack {
 	
     private void initListView() {
 		// TODO Auto-generated method stub
-		ListView lv=(ListView)findViewById(R.id.lv);
-		List<Map<String,Object>> data=new ArrayList<Map<String,Object>>();
+		lv=(ListView)findViewById(R.id.lv);
+		
 		
 		String historyRecords = OperatingSP.getString(mContext, OperatingSP.PREFERENCE_SEARCH_RECORD, OperatingSP.PREFERENCE_SEARCH_RECORD_DEFAULT);
 		if(historyRecords != null &&!historyRecords.equals(""))
 		{
-			String[] arrayRecords = historyRecords.split("_");  //用_当分割符
+			String[] arrayRecords = historyRecords.split("#");  //用_当分割符
 		
+			history_data.clear();
 			for(int i = 0;i < arrayRecords.length;i++)
 			{
 				Map<String,Object> map1=new HashMap<String, Object>();
 				map1.put("item_history",arrayRecords[i]);
-				data.add(map1);
+				history_data.add(map1);
 			}
 	
+		}
+		
+		if(history_data.size() == 0)
+		{
+			bt_search_clear_his.setVisibility(View.GONE);
+		}
+		else
+		{
+			bt_search_clear_his.setVisibility(View.VISIBLE);
 		}
 		lv.setOnItemClickListener(new OnItemClickListener()
 		{
@@ -123,7 +170,7 @@ public class SearchActivity extends ActivityPack {
 			}
 		});
 		
-		simpleAdapter = new SimpleAdapter(this, data, R.layout.item_serach_history_list, new String[]{"item_history"}, new int[]{R.id.tv_search_item});
+		simpleAdapter = new SimpleAdapter(this, history_data, R.layout.item_serach_history_list, new String[]{"item_history"}, new int[]{R.id.tv_search_item});
 		lv.setAdapter(simpleAdapter);
 		
 		 setListViewHeightBasedOnChildren(lv);   
@@ -137,9 +184,9 @@ public class SearchActivity extends ActivityPack {
         if (listAdapter == null) {   
             return;   
         }   
-   
+        Utils.Log("history_data.size()  = :"+history_data.size());
         int totalHeight = 0;   
-        for (int i = 0, len = listAdapter.getCount(); i < len; i++) {   
+        for (int i = 0, len = history_data.size(); i < len; i++) {   
             // listAdapter.getCount()返回数据项的数目   
             View listItem = listAdapter.getView(i, null, listView);   
             // 计算子项View 的宽高   
@@ -149,7 +196,8 @@ public class SearchActivity extends ActivityPack {
         }   
    
         ViewGroup.LayoutParams params = listView.getLayoutParams();   
-        params.height = totalHeight+ (listView.getDividerHeight() * (listAdapter.getCount() - 1));   
+        params.height = totalHeight+ (listView.getDividerHeight() * (history_data.size() - 1));   
+        Utils.Log("params.height  = :"+params.height);
         // listView.getDividerHeight()获取子项间分隔符占用的高度   
         // params.height最后得到整个ListView完整显示需要的高度   
         listView.setLayoutParams(params);   
